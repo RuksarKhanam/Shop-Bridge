dd825829070c2b52de63491b3e1fd898616f3c48
DESKTOP-IHQFOAE\ruksh <rukshar.khanam@centralogic.net>
2022-06-19 13:39:28 +0530

    minor

--- Shopbridge/Shopbridge/Program.cs
+++ Shopbridge/Shopbridge/Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
@@ -14,14 +15,12 @@ namespace Shopbridge
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            BuildWebHost(args).Run();
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
+        public static IWebHost BuildWebHost(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+               .UseStartup<Startup>()
+                .Build();
     }
 }
